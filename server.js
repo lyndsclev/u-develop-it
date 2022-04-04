1dd6cedea65598bb4a59ac422369f34e8e2824de
lyndsclev <lyndsclev@gmail.com>
2022-04-04 14:36:00 -0400

    created first query

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2'); 
+
 const express = require('express'); 
 
 const PORT = process.env.PORT || 3001; 
@@ -6,6 +8,21 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
+
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'election'
+    }, 
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (not found) 
 app.use((req, res) => {
